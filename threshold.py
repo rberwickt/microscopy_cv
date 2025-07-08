1c3a6da6a68873bc572646d1c5e8850ea9dfd7c9
rberwickt <97850329+rberwickt@users.noreply.github.com>
2025-07-08 11:52:08 -0500

    Create rough true masks

--- threshold.py
+++ threshold.py
@@ -1,15 +1,15 @@
 import cv2
 import matplotlib.pyplot as plt
+import os
 
 
-
-def thresh(path):
+def thresh(path, value):
     image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
     image = cv2.GaussianBlur(image,(5,5),0)
 
     #global threshold
-    threshold = 18
-    thresh_value, thresh_image = cv2.threshold(image,threshold,255,cv2.THRESH_BINARY)
+    
+    thresh_value, thresh_image = cv2.threshold(image,value,255,cv2.THRESH_BINARY)
 
 
     # Plot the original and filtered images
@@ -28,8 +28,15 @@ def thresh(path):
     #only writes the images to file after you close the graph window
     cv2.imwrite(path[:-4] + ".jpg", thresh_image)
 
+path = "./dataset"
+files = []
+for file in os.listdir(path):
+    if file[-4:] == ".png":
+        files.append(file)
+        thresh(path + "/" + file, 18) # 18 seems to work pretty well
+        # will likely have to tailor true masks eventually
 
-paths = ("./dataset/region1_FOV.PNG", "./dataset/region2_FOV.PNG",
-            "./dataset/region3_FOV.PNG", "./dataset/region4_FOV.png")
-for path in paths:
-    thresh(path)
+# ['FOV1.png', 'FOV2.png', 'FOV3.png', 'FOV4.png', 'Region1_FOV (2).png', 
+#  'Region2_3_FOV.png', 'Region4_FOV (2).png', 'region4_FOV.png', 'Region5_FOV.png', 
+#  'Region6_FOV.png', 'Region7_FOV.png']
+
